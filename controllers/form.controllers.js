bb521d4f0c3b86ab618dd184e3a4587f38a47068
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2023-05-01 18:25:00 -0600

    add 2.1 router

--- controllers/form.controllers.js
+++ controllers/form.controllers.js
@@ -1,5 +1,6 @@
 import Form from "../models/form.model.js";
 import { encryptFileName } from "../securityUtils/security.js";
+import mongoose from "mongoose";
 
 /**
  * This module contains the FormController class responsible for handling Form-related HTTP requests and responses.
@@ -11,51 +12,52 @@ import { encryptFileName } from "../securityUtils/security.js";
  */
 
 class FormController extends BaseController {
-  /**
-   * Creates a new instance of FormController.
-   *
-   * @constructor
-   */
-  constructor() {
-    super(Form, "Request");
-  }
-  /**
-   * Creates a new Form instance with encrypted file names for the uploaded documents.
-   *
-   * @function createForm
-   * @async
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @returns {object} - Returns the created Form object with status code 201 on success.
-   * @throws {object} - Throws an error object with status code 404 on failure.
-   */
-  async createForm(req, res) {
-    try {
-      // Encrypts the file names of the uploaded documents and creates a new Form instance with them.
-      req.files.map((file, index) => {
-        let newBody = req.body;
-        if (index === 0) {
-          const ineDoc = encryptFileName(file.filename);
-          newBody = { ...req.body, ineDoc };
+    /**
+     * Creates a new instance of FormController.
+     *
+     * @constructor
+     */
+    constructor() {
+            super(Form, "Request");
         }
-        if (index === 1) {
-          const addressDoc = encryptFileName(file.filename);
-          newBody = { ...newBody, addressDoc };
+        /**
+         * Creates a new Form instance with encrypted file names for the uploaded documents.
+         *
+         * @function createForm
+         * @async
+         * @param {object} req - Express request object.
+         * @param {object} res - Express response object.
+         * @returns {object} - Returns the created Form object with status code 201 on success.
+         * @throws {object} - Throws an error object with status code 404 on failure.
+         */
+    async createForm(req, res) {
+        try {
+            // Encrypts the file names of the uploaded documents and creates a new Form instance with them.
+            req.files.map((file, index) => {
+                let newBody = req.body;
+                if (index === 0) {
+                    const ineDoc = encryptFileName(file.filename);
+                    newBody = {...req.body, ineDoc };
+                }
+                if (index === 1) {
+                    const addressDoc = encryptFileName(file.filename);
+                    newBody = {...newBody, addressDoc };
+                }
+                if (index === 2) {
+                    const curpDoc = encryptFileName(file.filename);
+                    newBody = {...newBody, curpDoc };
+                }
+                if (index === 3) {
+                    const extraDoc = encryptFileName(file.filename);
+                    newBody = {...newBody, extraDoc };
+                }
+            });
+            const newForm = await Form.create(newBody);
+            await Form.updateOne({ _id: newForm._id }, { status: "En Proceso" });
+            const items = await Form.find();
+            res.status(201).json(items);
+        } catch (err) {
+            res.status(404).send({ message: err.message });
         }
-        if (index === 2) {
-          const curpDoc = encryptFileName(file.filename);
-          newBody = { ...newBody, curpDoc };
-        }
-        if (index === 3) {
-          const extraDoc = encryptFileName(file.filename);
-          newBody = { ...newBody, extraDoc };
-        }
-      });
-      await Form.create(newBody);
-      const items = await Form.find();
-      res.status(201).json(items);
-    } catch (err) {
-      res.status(404).send({ message: err.message });
     }
-  }
 }
