809502b8b8b64beedd6be31346f12bb0a4abc9c9
MandukoGR <gurom84@outlook.com>
2023-05-08 18:50:07 -0600

    hot fix

--- middlewares/abac.js
+++ middlewares/abac.js
@@ -4,22 +4,20 @@
  * @return A middleware function that checks if the user making the request has the specified role.
  */
 import User from "../models/user.model.js";
-export const limitRole = (role) =>  async (req, res, next) => {
-    // Find the user making the request and populate their role.
-    const user =  await User.findById(req.body.id).populate("ptrRol");
-    console.log(user)
-    // Get the user's role.
-    const rol = user.ptrRol.rol;
-    
-    // Check if the user's role matches the allowed role.
-    if (rol != role) {
-        // If the user's role doesn't match, return a 401 Unauthorized error.
-        return res.status(401).send({
-            message: "No tiene permisos para realizar esta acción",
-        });
-    } else {
-        // If the user's role matches, call the next middleware function.
-        next();
-    }
+export const limitRole = (role) => async (req, res, next) => {
+  // Find the user making the request and populate their role.
+  const user = await User.findById(req.body.id).populate("ptrRol");
+  // Get the user's role.
+  const rol = user.ptrRol.rol;
 
+  // Check if the user's role matches the allowed role.
+  if (rol != role) {
+    // If the user's role doesn't match, return a 401 Unauthorized error.
+    return res.status(401).send({
+      message: "No tiene permisos para realizar esta acción",
+    });
+  } else {
+    // If the user's role matches, call the next middleware function.
+    next();
+  }
 };
